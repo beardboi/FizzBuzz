43fb94d8255b326b4be2d24fffe1de70dca95292
beardboi <mr.k3rn3lp4n1c@gmail.com>
2021-04-25 16:41:02 -0400

    initial commit

---
+++ fizzbuzz.test.js
@@ -0,0 +1,37 @@
+/*
+    Copyright 2020 Diego Bravo <diego.bravo@alumnos.ucn.cl>
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+const fizzbuzz = require('./fizzbuzz');
+
+describe('fizzbuzz', () => {
+    test('Should be print 1 if it received 1.', () => {
+        const expected = 1;
+        const result = fizzbuzz(1);
+        expect(expected).toBe(result);
+    });
+
+    test('Should be print fizz if it received a number divisible by 3.', () => {
+        const expected = 'fizz';
+        const result = fizzbuzz(9);
+        expect(expected).toBe(result);
+    });
+
+    test('Should be print fizz if it received a number divisible by 5.', () => {
+        const expected = 'buzz';
+        const result = fizzbuzz(10);
+        expect(expected).toBe(result);
+    });
+
+    test('Should be print fizzbuzz if it received a number divisible by 3 and 5.', () => {
+        const expected = 'fizzbuzz';
+        const result = fizzbuzz(15);
+        expect(expected).toBe(result);
+    });
+});
